e3cbcbe9750e84f2c1fb8bd1138b278b026c65f3
nrps55 <81301486+nrps55@users.noreply.github.com>
2021-04-02 23:25:48 +0200

    Update load_tfds.sh

--- datasets/tf-new-datasets/load_tfds.sh
+++ datasets/tf-new-datasets/load_tfds.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --partition=graceGPU #Name of the GPUs partition
-#SBATCH -x ethnode[22,23,33,34]
+#SBATCH --partition= #Name of the GPUs partition
 #SBATCH --mem=3G
-#SBATCH -o /scratch/palacios/tests/log_dt.out #Standard output from the program
-#SBATCH -e /scratch/palacios/tests/log_dt.err #Standard error from the program
+#SBATCH -o  #Standard output from the program
+#SBATCH -e  #Standard error from the program
 #SBATCH -J load_tfds #Name of your program
 #SBATCH --array 0-2 #create 3 jobs
 source activate amluc
@@ -13,4 +12,4 @@ params=(\
 'savana_scenes.py' \
 'brazildam_sentinel.py'\
 )
-srun tfds build --data_dir='/scratch/palacios/data' ${params[$SLURM_ARRAY_TASK_ID]}
+srun tfds build --data_dir='/data' ${params[$SLURM_ARRAY_TASK_ID]}
